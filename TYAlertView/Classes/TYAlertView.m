c66c23bc1085197eae981e00312b7171210fa530
luckytianyiyan <luckytianyiyan@gmail.com>
2016-09-25 00:02:32 +0800

    M TYAlertView layout

--- TYAlertView/Classes/TYAlertView.m
+++ TYAlertView/Classes/TYAlertView.m
@@ -46,6 +46,7 @@ static CGFloat const kTYAlertViewDefaultShadowRadius = 4.f;
 {
     UIButton *button = [UIButton buttonWithType:UIButtonTypeSystem];
     [button setTitle:self.text forState:UIControlStateNormal];
+    button.translatesAutoresizingMaskIntoConstraints = NO;
     return button;
 }
 
@@ -54,11 +55,12 @@ static CGFloat const kTYAlertViewDefaultShadowRadius = 4.f;
 @interface TYAlertView()
 
 @property (nonatomic, strong) UIView *containerView;
+@property (nonatomic, strong) UIView *contentView;
 @property (nonatomic, strong) UILabel *titleLabel;
 @property (nonatomic, strong) UILabel *messageLabel;
 @property (nonatomic, strong) NSMutableArray<UIButton *> *buttons;
 @property (nonatomic, strong) NSMutableArray<TYAlertViewButtonItem *> *items;
-
+@property (nonatomic, strong) NSArray<NSLayoutConstraint *> *buttonConstraints;
 @end
 
 @implementation TYAlertView
@@ -103,31 +105,8 @@ static CGFloat const kTYAlertViewDefaultShadowRadius = 4.f;
 - (void)layoutSubviews
 {
     [super layoutSubviews];
-    CGFloat height = kTYAlertViewContentViewHeight;
-    CGFloat left = (CGRectGetWidth(self.bounds) - kTYAlertViewContentViewWidth) / 2.f;
-    CGFloat top = (CGRectGetHeight(self.bounds) - height) / 2.f;
     self.containerView.transform = CGAffineTransformIdentity;
-    self.containerView.frame = CGRectMake(left, top, kTYAlertViewContentViewWidth, height);
     self.containerView.layer.shadowPath = [UIBezierPath bezierPathWithRoundedRect:self.containerView.bounds cornerRadius:self.containerView.layer.cornerRadius].CGPath;
-    
-    CGFloat contentWidth = kTYAlertViewContentViewWidth - 2 * kTYAlertViewContentViewPaddingHorizontal;
-    
-    self.titleLabel.frame = CGRectMake(kTYAlertViewContentViewPaddingHorizontal, kTYAlertViewContentViewPaddingVertical, contentWidth, kTYAlertViewTitleLabelHeight);
-    
-    if (self.buttons.count == 2) {
-        CGFloat buttonWidth = contentWidth / 2;
-        UIButton *leftButton = [self.buttons firstObject];
-        leftButton.frame = CGRectMake(kTYAlertViewContentViewPaddingHorizontal, height - kTYAlertViewContentViewPaddingVertical - kTYAlertViewButtonHeight, buttonWidth, kTYAlertViewButtonHeight);
-        UIButton *rightButton = [self.buttons lastObject];
-        rightButton.frame = CGRectMake(kTYAlertViewContentViewPaddingHorizontal + buttonWidth, height - kTYAlertViewContentViewPaddingVertical - kTYAlertViewButtonHeight, buttonWidth, kTYAlertViewButtonHeight);
-    } else {
-        for (NSInteger i = 0; i < self.buttons.count; ++i) {
-            UIButton *button = self.buttons[i];
-            button.frame = CGRectMake(kTYAlertViewContentViewPaddingHorizontal, height - kTYAlertViewContentViewPaddingVertical - (i + 1) * kTYAlertViewButtonHeight, contentWidth, kTYAlertViewButtonHeight);
-        }
-    }
-    
-    self.messageLabel.frame = CGRectMake(kTYAlertViewContentViewPaddingHorizontal, kTYAlertViewTitleLabelHeight + kTYAlertViewContentViewPaddingVertical, contentWidth, height - kTYAlertViewTitleLabelHeight - 2 * kTYAlertViewContentViewPaddingVertical - self.buttons.count * kTYAlertViewButtonHeight);
 }
 
 - (void)show
@@ -149,6 +128,35 @@ static CGFloat const kTYAlertViewDefaultShadowRadius = 4.f;
     button.tag = index;
     [self.containerView addSubview:button];
     [self.buttons addObject:button];
+    
+    [self.containerView removeConstraints:_buttonConstraints];
+    NSMutableArray *buttonConstraints;
+    if (self.buttons.count == 2) {
+        UIButton *leftButton = [self.buttons firstObject];
+        UIButton *rightButton = [self.buttons lastObject];
+        UIView *contentView = self.contentView;
+        buttonConstraints = [NSMutableArray arrayWithCapacity:5];
+        [buttonConstraints addObjectsFromArray:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[contentView]-[leftButton]-|" options:0 metrics:nil views:NSDictionaryOfVariableBindings(contentView, leftButton)]];
+        [buttonConstraints addObjectsFromArray:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[contentView]-[rightButton]-|" options:0 metrics:nil views:NSDictionaryOfVariableBindings(contentView, rightButton)]];
+        [buttonConstraints addObjectsFromArray:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-[leftButton]-[rightButton(==leftButton)]-|" options:0 metrics:nil views:NSDictionaryOfVariableBindings(leftButton, rightButton)]];
+    } else {
+        NSMutableString *visualFormat = [NSMutableString stringWithString:@"V:[contentView]"];
+        buttonConstraints = [NSMutableArray arrayWithCapacity:self.buttons.count * 2];
+        NSMutableDictionary *views = [NSMutableDictionary dictionaryWithCapacity:self.buttons.count + 1];
+        [views setObject:self.contentView forKey:@"contentView"];
+        for (NSInteger i = 0; i < self.buttons.count; ++i) {
+            UIButton *button = self.buttons[i];
+            NSString *name = [NSString stringWithFormat:@"button%zd", i];
+            [visualFormat appendFormat:@"-[%@(>=44)]", name];
+            [views setObject:button forKey:name];
+            [buttonConstraints addObjectsFromArray:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-[button]-|" options:0 metrics:nil views:NSDictionaryOfVariableBindings(button)]];
+        }
+        [visualFormat appendString:@"-|"];
+        [buttonConstraints addObjectsFromArray:[NSLayoutConstraint constraintsWithVisualFormat:visualFormat options:0 metrics:nil views:views]];
+    }
+    _buttonConstraints = buttonConstraints;
+    [self.containerView addConstraints:_buttonConstraints];
+    
     return index;
 }
 
@@ -177,15 +185,57 @@ static CGFloat const kTYAlertViewDefaultShadowRadius = 4.f;
     _containerView.layer.shadowRadius = self.shadowRadius;
     _containerView.layer.shadowOpacity = .5f;
     [self addSubview:_containerView];
+    _containerView.translatesAutoresizingMaskIntoConstraints = NO;
+    [self addConstraint:[NSLayoutConstraint constraintWithItem:_containerView attribute:NSLayoutAttributeCenterX relatedBy:NSLayoutRelationEqual toItem:self attribute:NSLayoutAttributeCenterX multiplier:1.f constant:0]];
+    [self addConstraint:[NSLayoutConstraint constraintWithItem:_containerView attribute:NSLayoutAttributeCenterY relatedBy:NSLayoutRelationEqual toItem:self attribute:NSLayoutAttributeCenterY multiplier:1.f constant:0]];
+    [_containerView addConstraint:[NSLayoutConstraint constraintWithItem:_containerView attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationEqual toItem:nil attribute:nil multiplier:1.f constant:270.f]];
+    [_containerView addConstraint:[NSLayoutConstraint constraintWithItem:_containerView attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationGreaterThanOrEqual toItem:nil attribute:nil multiplier:1.f constant:44.f]];
     
-    // lazy
-    [_containerView addSubview:self.titleLabel];
+    // Content View
+    _contentView = [[UIView alloc] init];
+    [_containerView addSubview:_contentView];
+    _contentView.translatesAutoresizingMaskIntoConstraints = NO;
+    [_containerView addConstraint:[NSLayoutConstraint constraintWithItem:_contentView attribute:NSLayoutAttributeLeading relatedBy:NSLayoutRelationEqual toItem:_containerView attribute:NSLayoutAttributeLeading multiplier:1.f constant:0]];
+    [_containerView addConstraint:[NSLayoutConstraint constraintWithItem:_contentView attribute:NSLayoutAttributeTrailing relatedBy:NSLayoutRelationEqual toItem:_containerView attribute:NSLayoutAttributeTrailing multiplier:1.f constant:0]];
+    [_containerView addConstraint:[NSLayoutConstraint constraintWithItem:_contentView attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:_containerView attribute:NSLayoutAttributeTop multiplier:1.f constant:0]];
+    NSLayoutConstraint *contentViewFullHeightConstraint = [NSLayoutConstraint constraintWithItem:_contentView attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationEqual toItem:_containerView attribute:NSLayoutAttributeHeight multiplier:1.f constant:0];
+    contentViewFullHeightConstraint.priority = UILayoutPriorityDefaultHigh;
+    [_containerView addConstraint:contentViewFullHeightConstraint];
     
-    // lazy
-    [_containerView addSubview:self.messageLabel];
+    // Title Label
+    UILabel *titleLabel = self.titleLabel;
+    [_contentView addSubview:titleLabel];
+    titleLabel.translatesAutoresizingMaskIntoConstraints = NO;
+    [titleLabel setContentHuggingPriority:UILayoutPriorityRequired forAxis:UILayoutConstraintAxisVertical];
+    [titleLabel setContentCompressionResistancePriority:UILayoutPriorityRequired forAxis:UILayoutConstraintAxisVertical];
+    
+    [_contentView addConstraint:[NSLayoutConstraint constraintWithItem:self.titleLabel attribute:NSLayoutAttributeCenterX relatedBy:NSLayoutRelationEqual toItem:_contentView attribute:NSLayoutAttributeCenterX multiplier:1.f constant:0]];
+    
+    // Message Label
+    UILabel *messageLabel = self.messageLabel;
+    [_contentView addSubview:messageLabel];
+    messageLabel.translatesAutoresizingMaskIntoConstraints = NO;
+    [messageLabel setContentHuggingPriority:UILayoutPriorityRequired forAxis:UILayoutConstraintAxisVertical];
+    [messageLabel setContentCompressionResistancePriority:UILayoutPriorityRequired forAxis:UILayoutConstraintAxisVertical];
+    [_contentView addConstraint:[NSLayoutConstraint constraintWithItem:self.messageLabel attribute:NSLayoutAttributeCenterX relatedBy:NSLayoutRelationEqual toItem:_contentView attribute:NSLayoutAttributeCenterX multiplier:1.f constant:0]];
+    
+    [_contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-16-[titleLabel]-16-|" options:0 metrics:nil views:NSDictionaryOfVariableBindings(titleLabel)]];
+    [_contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-16-[messageLabel]-16-|" options:0 metrics:nil views:NSDictionaryOfVariableBindings(messageLabel)]];
+    [_contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-20-[titleLabel]-4-[messageLabel]-20-|" options:0 metrics:nil views:NSDictionaryOfVariableBindings(titleLabel, messageLabel)]];
+    
     
     _buttons = [NSMutableArray array];
     _items = [NSMutableArray array];
+    
+#warning test
+    
+//    UIButton *button = [UIButton buttonWithType:UIButtonTypeSystem];
+//    button.translatesAutoresizingMaskIntoConstraints = NO;
+//    [button setTitle:@"OK" forState:UIControlStateNormal];
+//    [_containerView addSubview:button];
+//    NSLayoutConstraint *t = [NSLayoutConstraint constraintWithItem:_containerView attribute:NSLayoutAttributeBottom relatedBy:NSLayoutRelationEqual toItem:button attribute:NSLayoutAttributeBottom multiplier:1.f constant:0];
+//    [_containerView addConstraint:t];
+//    [self.containerView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[_contentView]-[button]-|" options:0 metrics:nil views:NSDictionaryOfVariableBindings(_contentView, button)]];
 }
 
 #pragma mark - Transition
