38829964d56acc5e77187d52647fc1868023b751
luckytianyiyan <luckytianyiyan@gmail.com>
2016-09-24 15:28:31 +0800

    M TYAlertBackgroundWindow add TYAlertViewBackgroundStyle

--- TYAlertView/Classes/TYAlertBackgroundWindow.h
+++ TYAlertView/Classes/TYAlertBackgroundWindow.h
@@ -8,8 +8,15 @@
 
 #import <UIKit/UIKit.h>
 
+typedef NS_ENUM(NSInteger, TYAlertViewBackgroundStyle) {
+    TYAlertViewBackgroundStyleSolid = 0,
+    TYAlertViewBackgroundStyleGradient,
+};
+
 extern const UIWindowLevel UIWindowLevelTYAlertBackground;
 
 @interface TYAlertBackgroundWindow : UIWindow
+
+@property (nonatomic, assign) TYAlertViewBackgroundStyle style;
 
 @end
--- TYAlertView/Classes/TYAlertBackgroundWindow.m
+++ TYAlertView/Classes/TYAlertBackgroundWindow.m
@@ -12,8 +12,6 @@ const UIWindowLevel UIWindowLevelTYAlertBackground = 1985.0;
 
 @interface TYAlertBackgroundWindow()
 
-@property (nonatomic, strong) CALayer *layer;
-
 @end
 
 @implementation TYAlertBackgroundWindow
@@ -26,6 +24,49 @@ const UIWindowLevel UIWindowLevelTYAlertBackground = 1985.0;
         self.windowLevel = UIWindowLevelTYAlertBackground;
     }
     return self;
+}
+
+- (void)drawRect:(CGRect)rect
+{
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    switch (self.style) {
+        case TYAlertViewBackgroundStyleGradient:
+        {
+            size_t locationsCount = 2;
+            CGFloat locations[2] = {0.0f, 1.0f};
+            CGFloat colors[8] = {0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.75f};
+            CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+            CGGradientRef gradient = CGGradientCreateWithColorComponents(colorSpace, colors, locations, locationsCount);
+            CGColorSpaceRelease(colorSpace);
+            
+            CGFloat width = CGRectGetWidth(self.bounds);
+            CGFloat height = CGRectGetHeight(self.bounds);
+            
+            CGPoint center = CGPointMake(width / 2, height / 2);
+            CGFloat radius = MIN(width, height);
+            CGContextDrawRadialGradient (context, gradient, center, 0, center, radius, kCGGradientDrawsAfterEndLocation);
+            CGGradientRelease(gradient);
+            break;
+        }
+        case TYAlertViewBackgroundStyleSolid:
+        {
+            [[UIColor colorWithWhite:0 alpha:0.5] set];
+            CGContextFillRect(context, self.bounds);
+            break;
+        }
+    }
+}
+
+#pragma mark - Setter / Getter
+
+- (void)setStyle:(TYAlertViewBackgroundStyle)style
+{
+    if (_style == style) {
+        return;
+    }
+    _style = style;
+    
+    [self setNeedsDisplay];
 }
 
 @end
